54100e77a2352ee281912291a3e783d853b7eaea
Anthony Degarimore <anthony@degarimore.com>
2025-12-23 12:44:02 -0800

    Refactor JWTAuth to simplify imports and replace httpx-specific references with generic equivalents

--- breezeway/models/auth.py
+++ breezeway/models/auth.py
@@ -2,13 +2,12 @@ import logging
 import time
 import typing
 
-import httpx
-from httpx import Request, Response
+from httpx import Auth, Request, Response
 
 from breezeway.errors import AuthenticationError, RateLimitExceeded, APIClientError
 
 
-class JWTAuth(httpx.Auth):
+class JWTAuth(Auth):
     HEADERS = {'accept': 'application/json'}
 
     def __init__(self, base_url: str, client_id: str, client_secret: str):
@@ -20,7 +19,7 @@ class JWTAuth(httpx.Auth):
         self._token_expires_at = 0
 
     @staticmethod
-    def _handle_response(resp: httpx.Response) -> None:
+    def _handle_response(resp: Response) -> None:
         if resp.json() is None: # Breezeway returns 200 with empty body when client_secret is incorrect
             raise AuthenticationError()
         if 'error' not in resp.json():
@@ -35,20 +34,20 @@ class JWTAuth(httpx.Auth):
     def authenticated(self) -> bool:
         return self._access_token is not None and self._refresh_token is not None
 
-    def build_authentication_request(self) -> httpx.Request:
+    def build_authentication_request(self) -> Request:
         url = self.base_url + '/public/auth/v1/'
         payload = {
             'client_id': self.client_id,
             'client_secret': self.client_secret
         }
-        return httpx.Request("POST", url, headers=self.HEADERS, json=payload)
+        return Request("POST", url, headers=self.HEADERS, json=payload)
 
-    def build_refresh_token_request(self) -> httpx.Request:
+    def build_refresh_token_request(self) -> Request:
         url = self.base_url + '/public/auth/v1/refresh'
         headers = self.HEADERS | {'authorization': f'JWT {self._refresh_token}'}
-        return httpx.Request("POST", url, headers=headers)
+        return Request("POST", url, headers=headers)
 
-    def _update_tokens(self, response: httpx.Response):
+    def _update_tokens(self, response: Response):
         response.raise_for_status()
         body = response.json()
         if not body or 'error' in body:
