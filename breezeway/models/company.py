5ffe17c3567398a8bcb853eaa71a32c20addbaaa
Anthony DeGarimore <anthony@degarimore.com>
2026-05-08 00:19:09 -0700

    Add Linens Department

--- breezeway/models/company.py
+++ breezeway/models/company.py
@@ -9,39 +9,41 @@ class Department(StrEnum):
     HOUSEKEEPING = 'housekeeping'
     INSPECTION = 'inspection'
     MAINTENANCE = 'maintenance'
-    SAFETY = 'safety'
-    MANAGEMENT = 'management'
-    OFFICE = 'office'
-    FINANCE = 'finance'
     ACCOUNTING = 'accounting'
-    PROPERTY_SERVICES = 'property_services'
-    GUEST_SERVICES = 'guest_services'
-    OWNER_SERVICES = 'owner_services'
-    VENDORS = 'vendors'
     CONCIERGE = 'concierge'
     EXPERIENCES = 'experiences'
-    RESERVATIONS = 'reservations'
+    FINANCE = 'finance'
+    GUEST_SERVICES = 'guest_services'
+    LINENS = 'linens'
     LOST_AND_FOUND = 'lost_and_found'
+    MANAGEMENT = 'management'
+    OFFICE = 'office'
+    OWNER_SERVICES = 'owner_services'
+    PROPERTY_SERVICES = 'property_services'
+    RESERVATIONS = 'reservations'
+    SAFETY = 'safety'
+    VENDORS = 'vendors'
 
     @property
     def name(self) -> str:
         return {
-            Department.HOUSEKEEPING: "Cleaning",
-            Department.INSPECTION: "Inspection",
-            Department.MAINTENANCE: "Maintenance",
-            Department.SAFETY: "Safety",
-            Department.MANAGEMENT: "Management",
-            Department.OFFICE: "Office",
-            Department.FINANCE: "Finance",
-            Department.ACCOUNTING: "Accounting",
-            Department.PROPERTY_SERVICES: "Property Services",
-            Department.GUEST_SERVICES: "Guest Services",
-            Department.OWNER_SERVICES: "Owner Services",
-            Department.VENDORS: "Vendors",
-            Department.CONCIERGE: "Concierge",
-            Department.EXPERIENCES: "Experiences",
-            Department.RESERVATIONS: "Reservations",
-            Department.LOST_AND_FOUND: "Lost and Found",
+            Department.HOUSEKEEPING: '"Cleaning',
+            Department.INSPECTION: 'Inspection',
+            Department.MAINTENANCE: 'Maintenance',
+            Department.ACCOUNTING: 'Accounting',
+            Department.CONCIERGE: 'Concierge',
+            Department.EXPERIENCES: 'Experiences',
+            Department.FINANCE: 'Finance',
+            Department.GUEST_SERVICES: 'Guest Services',
+            Department.LINENS: 'Linens',
+            Department.LOST_AND_FOUND: 'Lost and Found',
+            Department.MANAGEMENT: 'Management',
+            Department.OFFICE: 'Office',
+            Department.OWNER_SERVICES: 'Owner Services',
+            Department.PROPERTY_SERVICES: 'Property Services',
+            Department.RESERVATIONS: 'Reservations',
+            Department.SAFETY: 'Safety',
+            Department.VENDORS: 'Vendors',
         }[self]
 
 
